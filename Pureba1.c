99425fc7c4d54f2f8b5dc456e84ce496a2da62d3
Mateo_Araque34 <131192048+Araque1218@users.noreply.github.com>
2023-05-02 12:26:41 -0500

    Se usaron comandos para poder determinar a la función que se realizaran operaciones matemáticas

---
+++ Pureba1.c
@@ -0,0 +1,10 @@
+//Se utiliza estas ordenes pagar dar a entender al programa que vamos a hacer operaciones matematicas y en este caso sumar digitos//
+
+#include <stdio.h>
+
+int sumar_digitos(int n);
+
+int main() {
+    int num, resultado;
+    
+    
